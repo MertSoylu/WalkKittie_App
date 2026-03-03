d5b918f44d57b2a0e9cee2ede118ddf5afb867b4
Mert <s6ylumert@gmail.com>
2026-03-03 20:47:51 +0300

    Change UI and add new coin & market system

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.mert.paticat"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.1.0"
+        versionCode = 10
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
